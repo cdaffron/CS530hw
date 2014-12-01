15b7887f7772dc9c2dc22cf93b0799e3ee183d20
Chris Daffron <cdaffron@vols.utk.edu>
2014-11-30 22:03:46 -0500

    book solution

---
+++ linux/book.cu
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <cuda.h>
+__global__ void life (unsigned char *d_board,int iterations) {
+    int i,row,col,rows,cols;
+    unsigned char state,neighbors;
+    row = blockIdx.y * blockDim.y + threadIdx.y;
+    col = blockIdx.x * blockDim.x + threadIdx.x;
+    rows = gridDim.y * blockDim.y;
+    cols = gridDim.x * blockDim.x;
+    state = d_board[(row)*cols+(col)];
+    for (i=0;i<iterations;i++) {
+        neighbors=0;
+        if (row!=0) {
+            if (col!=0) if (d_board[(row-1)*cols+(col-1)]==1) neighbors++;
+            if (d_board[(row-1)*cols+(col)]==1) neighbors++;
+            if (col!=(cols-1)) if (d_board[(row-1)*cols+(col+1)]==1) neighbors++;
+        }
+        if (col!=0) if (d_board[(row)*cols+(col-1)]==1) neighbors++;
+        if (col!=(cols-1)) if (d_board[(row)*cols+(col+1)]==1) neighbors++;
+        if (row!=(rows-1)) {
+            if (col!=0) if (d_board[(row+1)*cols+(col-1)]==1) neighbors++;
+            if (d_board[(row+1)*cols+(col)]==1) neighbors++;
+            if (col!=(cols-1)) if (d_board[(row+1)*cols+(col+1)]==1) neighbors++;
+        }
+        if (neighbors<2) state = 0;
+        else if (neighbors==3) state = 1;
+        else if (neighbors>3) state = 0;
+        __syncthreads();
+        d_board[(row)*cols+(col)]=state;
+    }
+}
+int main () {
+    dim3 gDim,bDim;
+    unsigned char *h_board,*d_board;
+    int i,iterations=100;
+    bDim.y=16;
+    bDim.x=32;
+    bDim.z=1;
+    
+    gDim.y=16;
+    gDim.x=8;
+    gDim.z=1;
+    h_board=(unsigned char *)malloc(sizeof(unsigned char)*256*256);
+    cudaMalloc((void **)&d_board,sizeof(unsigned char)*256*256);
+    srand(0);
+    for (i=0;i<256*256;i++) h_board[i]=rand()%2;
+    cudaMemcpy(d_board,h_board,sizeof(unsigned char)*256*256,cudaMemcpyHostToDevice);
+    life <<<gDim,bDim>>> (d_board,iterations);
+    cudaMemcpy(h_board,d_board,sizeof(unsigned char)*256*256,cudaMemcpyDeviceToHost);
+    free(h_board);
+    cudaFree(d_board);
+}
